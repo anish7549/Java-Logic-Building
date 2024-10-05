3f9f87aa22ae57582495d27ee8bd6e5872ede533
Anish Kumar <anish2003singh@gmail.com>
2024-10-06 01:04:12 +0530

    anish

---
+++ Array/AlternateArray.java
@@ -0,0 +1,9 @@
+public class AlternateArray {
+    public static void main(String[] args){
+        int [] arr = {45,18,7,8,1,17,63};
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i]+ " ");
+            i++;
+        }
+    }
+}
